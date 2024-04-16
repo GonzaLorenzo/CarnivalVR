4882d78acee5476e47f390bdd9aa5986a4b41803
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2024-04-16 02:37:23 -0300

    New Weapon and Anims

--- Assets/Scripts/ToyGun.cs
+++ Assets/Scripts/ToyGun.cs
@@ -1,28 +1,36 @@
-using System.Net.NetworkInformation;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ToyGun : MonoBehaviour
 {
-    [SerializeField] private Transform _shootPos;
+    private Animator _gunAnimator;
+    private AudioSource _audioSource;
+
+    [Header("Prefabs")]
+    public GameObject casingPrefab;
+    public GameObject muzzleFlashPrefab;
     public GameObject oldSphere;
 
+    [Header("Settings")]
+    [SerializeField] private float _casingEjectPower = 150f;
+    [SerializeField] private float _muzzleFlashTimer = 2f;
+    
+    [Header("Location Refrences")]
+    [SerializeField] private Transform _shootPos;
+    [SerializeField] private Transform _casingExitLocation;
+
+    [Header("AudioClips")]
+    [SerializeField] private AudioClip _shootSound;
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _gunAnimator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void Shoot()
     {
-        Debug.Log("Hola putos");
-
+        //_gunAnimator.SetTrigger("Fire");
+        _gunAnimator.Play("Fire");
         RaycastHit hit;
 
         if (Physics.Raycast(_shootPos.position, _shootPos.forward, out hit, 1000))
@@ -33,5 +41,33 @@ public class ToyGun : MonoBehaviour
 
             Instantiate(oldSphere, hit.point, Quaternion.identity);
         }
+    }
+
+    public void Fire()
+    {
+        _audioSource.PlayOneShot(_shootSound);
+        if (muzzleFlashPrefab)
+        {
+            GameObject tempFlash;
+            tempFlash = Instantiate(muzzleFlashPrefab, _shootPos.position, _shootPos.rotation);
+
+            Destroy(tempFlash, _muzzleFlashTimer);
+        }
+    }
+
+    void CasingRelease()
+    {
+        if (!_casingExitLocation || !casingPrefab)
+        { return; }
+
+        GameObject casing;
+        casing = Instantiate(casingPrefab, _casingExitLocation.position, _casingExitLocation.rotation) as GameObject;
+
+        //Add force on casing to push it out
+        casing.GetComponent<Rigidbody>().AddExplosionForce(Random.Range(_casingEjectPower * 0.7f, _casingEjectPower), (_casingExitLocation.position - _casingExitLocation.right * 0.3f - _casingExitLocation.up * 0.6f), 1f);
+        //Add torque to make casing spin in random direction
+        casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(100f, 1000f)), ForceMode.Impulse);
+
+        //Destroy(tempCasing, destroyTimer);
     }
 }
